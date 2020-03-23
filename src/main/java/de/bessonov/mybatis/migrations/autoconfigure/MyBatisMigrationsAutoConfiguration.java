204887dee1ea7a563dad85a8cf90ad133e9f1818
benoitl <benoni29@gmail.com>
2020-03-23 17:15:41 +0100

    spring boot 2 support

--- src/main/java/de/bessonov/mybatis/migrations/autoconfigure/MyBatisMigrationsAutoConfiguration.java
+++ src/main/java/de/bessonov/mybatis/migrations/autoconfigure/MyBatisMigrationsAutoConfiguration.java
@@ -14,10 +14,10 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.data.jpa.EntityManagerFactoryDependsOnPostProcessor;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
